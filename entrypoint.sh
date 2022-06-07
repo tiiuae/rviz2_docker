83b32115d7c89b70a2d057c7ff6331ca5e25d2da
Daniel Smrcka <smrckda1@fel.cvut.cz>
2022-06-07 10:43:22 +0200

    added docker files

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /opt/ros/galactic/setup.bash
+
+# start X11 server
+Xvfb :0 -screen 0 1920x1080x24 &
+
+# start VNC server
+x11vnc -display :0 -rfbport 5900 -forever -xkb -noxrecord -noxfixes -nopw -wait 5 -shared &
+
+# start window manager
+HOME=/root DISPLAY=:0 USER=root openbox
