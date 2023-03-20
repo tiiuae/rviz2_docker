8bcabb5e8485d80e85e3996db4ca4cb4b6169ea0
stibipet <stibipet@fel.cvut.cz>
2023-03-20 10:12:45 +0100

    update entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
-FROM ghcr.io/tiiuae/fog-ros-baseimage-builder:sha-11bedfa
+FROM ghcr.io/tiiuae/fog-ros-baseimage:sha-11bedfa
+
+RUN apt-get update -y && apt-get install -y \
+  ros-humble-rviz2
 
 COPY scripts /scripts
 COPY rviz /rviz
 COPY config/menu.xml /etc/xdg/openbox/menu.xml
 
-RUN apt update
-
 ENV PATH=${PATH}:/scripts
 
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT /entrypoint.sh
-
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,28 @@
-#!/bin/bash
-source /opt/ros/galactic/setup.bash
+#!/bin/bash -e
+
+_term() {
+	# FILL UP PROCESS SEARCH PATTERN HERE TO FIND PROPER PROCESS FOR SIGINT:
+	pattern="component_container_mt"
+
+	pid_value="$(ps -ax | grep $pattern | grep -v grep | awk '{ print $1 }')"
+	if [ "$pid_value" != "" ]; then
+		pid=$pid_value
+		echo "Send SIGINT to pid $pid"
+	else
+		pid=1
+		echo "Pattern not found, send SIGINT to pid $pid"
+	fi
+	kill -s SIGINT $pid
+}
+# Use SIGTERM or TERM, does not seem to make any difference.
+trap _term TERM
+
+ROS_FLAGS=""
+if [[ ${SIMULATION+x} != "" ]]; then
+	ROS_FLAGS="use_sim_time:=true ${ROS_FLAGS}"
+fi
+
+source /opt/ros/humble/setup.bash
 
 # make VNC port configurable (prepare for moving to a dedicated port)
 vnc_port="${VNC_PORT:=3000}"
